635f04ef6a1ef448b3e24e82925969ebb6dbfaed
neighbor-peace <mcostello91@gmail.com>
2022-11-01 10:38:09 -0700

    verify back-end is connected

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
   render() {
+    function testBackend() {
+      axios
+        .get('/api')
+        .then((response) => {
+          console.log({ response });
+        })
+        .catch((err) => {
+          console.log({ err });
+        });
+    }
     return (
       <div>
         <h1>Hello World!</h1>
+        <button onClick={testBackend}>Test Backend</button>
       </div>
     );
   }
--- client/index.js
+++ client/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
 import styles from './styles.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+root.render(<App tab='home' />);
