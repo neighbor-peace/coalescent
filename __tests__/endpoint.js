b495a32b4ca5c7edef3ddc6bd717bd53ac79aa6e
neighbor-peace <mcostello91@gmail.com>
2023-02-06 19:01:18 -0800

    add error handling tests for signup route

--- __tests__/endpoint.js
+++ __tests__/endpoint.js
@@ -3,36 +3,95 @@ const dotenv = require('dotenv');
 const { Str } = require('@supercharge/strings');
 dotenv.config();
 
-const url = 'http://localhost:8080';
+const server = 'http://localhost:8080';
 
 describe('Route integration', () => {
-  describe('/healthCheck', () => {
+  describe('/', () => {
     describe('GET', () => {
-      it('responds with 200 status', () => {
-        return request(url).get('/api/healthCheck').expect(200);
+      it('responds with 200 status and text/html content type', async () => {
+        const response = await request(server).get('/');
+        expect(response.headers['content-type']).toMatch(/text\/html/);
+        expect(response.status).toEqual(200);
       });
     });
   });
 
-  describe('/user', () => {
-    describe('POST /signup', () => {
-      it('responds with 200 status and application/json content type', () => {
+  describe('/api', () => {
+    describe('/user/signup', () => {
+      describe('POST', () => {
         const randomStr = Str.random(50);
-        return request(url)
-          .post('/api/user/signup')
-          .send({
-            firstName: `firstName${randomStr}`,
-            lastName: `lastName${randomStr}`,
-            username: `username${randomStr}`,
-            password: `password${randomStr}`,
-            team: `team${randomStr}`,
-            isAdmin: false,
-          })
-          .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(200);
+        it('responds with 200 status and application/json content type', () => {
+          return request(server)
+            .post('/api/user/signup')
+            .send({
+              firstName: `firstName${randomStr}`,
+              lastName: `lastName${randomStr}`,
+              username: `username${randomStr}`,
+              password: `password${randomStr}`,
+              team: `team${randomStr}`,
+              isAdmin: false,
+            })
+            .expect('Content-Type', /json/)
+            .expect(200);
+        });
+
+        it('responds with 400 status when user data is incomplete', async () => {
+          const missingFirstNameRes = await request(server)
+            .post('/api/user/signup')
+            .send({
+              lastName: `lastName${randomStr}`,
+              username: `username${randomStr}`,
+              password: `password${randomStr}`,
+              team: `team${randomStr}`,
+              isAdmin: false,
+            });
+          const missingLastNameRes = await request(server)
+            .post('/api/user/signup')
+            .send({
+              firstName: `firstName${randomStr}`,
+              username: `username${randomStr}`,
+              password: `password${randomStr}`,
+              team: `team${randomStr}`,
+              isAdmin: false,
+            });
+          const missingUsernameRes = await request(server)
+            .post('/api/user/signup')
+            .send({
+              firstName: `firstName${randomStr}`,
+              lastName: `lastName${randomStr}`,
+              password: `password${randomStr}`,
+              team: `team${randomStr}`,
+              isAdmin: false,
+            });
+          const missingPwdRes = await request(server)
+            .post('/api/user/signup')
+            .send({
+              firstName: `firstName${randomStr}`,
+              lastName: `lastName${randomStr}`,
+              username: `username${randomStr}`,
+              team: `team${randomStr}`,
+              isAdmin: false,
+            });
+          expect(missingFirstNameRes.status).toEqual(400);
+          expect(missingLastNameRes.status).toEqual(400);
+          expect(missingUsernameRes.status).toEqual(400);
+          expect(missingPwdRes.status).toEqual(400);
+        });
+
+        it('responds with 409 status when duplicate username is submitted', () => {
+          return request(server)
+            .post('/api/user/signup')
+            .send({
+              firstName: `firstName${randomStr}`,
+              lastName: `lastName${randomStr}`,
+              username: `username${randomStr}`,
+              password: `password${randomStr}`,
+              team: `team${randomStr}`,
+              isAdmin: false,
+            })
+            .expect(409);
+        });
       });
-      it.todo('catches bad usernames');
     });
   });
 });
