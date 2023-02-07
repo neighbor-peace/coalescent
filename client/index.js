c2075fd25254f4bce8656a797499010252c17aa9
neighbor-peace <mcostello91@gmail.com>
2023-02-06 18:16:03 -0800

    add styles module and update title

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { FluentProvider, teamsLightTheme } from '@fluentui/react-components';
+import styles from './styles.css';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
