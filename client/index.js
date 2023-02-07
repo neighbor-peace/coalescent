7b781fea02903f1857087d80e8dff3b92a60ca54
neighbor-peace <mcostello91@gmail.com>
2023-02-06 18:14:32 -0800

    remove unused modules

--- client/index.js
+++ client/index.js
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { FluentProvider, teamsLightTheme } from '@fluentui/react-components';
-import styles from './styles.css';
-// import reset from './reset.css';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
